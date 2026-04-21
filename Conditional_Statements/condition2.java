7170c82a854d2615fa8a3c2304a82e5f4edc69ef
Khushi Kumari <thenameiskhushi07@gmail.com>
2026-04-21 21:56:06 +0530

    Day03 Completed

---
+++ Conditional_Statements/condition2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class condition2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if (x % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+        sc.close();
+    }
+
+}
